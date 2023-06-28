ef6413905ab9e70bd8349599da9e1f5fbef42934
ICdalia <idalia.cieciura@onet.pl>
2023-06-28 17:06:33 +0200

    components home cd

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import HomeHeader from "./homeHeader.js";
+import HomeMenu from "./homeMenu.js";
 
 const Home = () => {
     return (
         <>
             <div>
              <HomeHeader/>
+             <HomeMenu/>
             </div>
 
         </>
--- src/components/home/homeHeader.js
+++ src/components/home/homeHeader.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import  {Link as ScrollLink} from "react-scroll";
 
 const homeHeader = () => {
     return (
@@ -8,19 +7,6 @@ const homeHeader = () => {
             <nav className="navbar_user">
                 <Link to="/logowanie">Zaloguj</Link>
                 <Link to="/rejestracja">Załóż konto</Link>
-            </nav>
-            <nav className="navbar_menu">
-                <ScrollLink
-                    activeClass="active"
-                    to="start"
-                    spy={true}
-                    smooth={true}
-                    offset={50}
-                    duration={500}
-                >
-                    Start
-                </ScrollLink>
-
             </nav>
         </div>
     )
