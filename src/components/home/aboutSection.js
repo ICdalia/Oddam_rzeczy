4093d06804c4dd24e9e8a7db872eb0bdfbb69931
ICdalia <idalia.cieciura@onet.pl>
2023-07-03 10:19:00 +0200

    section about

---
+++ src/components/home/aboutSection.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../stylesHome.scss"
+import AboutImage from "./aboutImage";
+import About from "./about";
+
+const AboutSection = () => {
+    return (
+        <div>
+            <AboutImage/>
+            <About/>
+        </div>
+    )
+};
+
+export default AboutSection;
