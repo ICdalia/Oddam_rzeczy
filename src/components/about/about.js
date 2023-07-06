6ad388d1fe3ab9ffd8fd923163f6eacf30d0cf75
ICdalia <idalia.cieciura@onet.pl>
2023-07-06 16:51:29 +0200

    section about

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../stylesHome.scss"
+import "../stylesHome.scss";
 import Decoration from "../../assets/Decoration.svg";
 import Signature from "../../assets/Signature.svg";
 import AboutImage from "./aboutImage";
@@ -8,18 +8,24 @@ const AboutUs = () => {
     return (
         <div className="container_about" id="about">
             <div className="about">
-                <h2 className="title_about">O nas</h2>
-                <div className="decoration_home">
-                    <img src={Decoration} alt="Decoration Home" />
+                <div className="columns">
+                    <div className="column-left">
+                        <h2 className="title_about">O nas</h2>
+                        <div className="decoration_home">
+                            <img src={Decoration} alt="Decoration Home" />
+                        </div>
+                        <p className="text_about">Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                        <div className="signature_home">
+                            <img src={Signature} alt="signature" />
+                        </div>
+                    </div>
+                    <div className="column-right">
+                        <AboutImage />
+                    </div>
                 </div>
-                <p className="text_about">Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
-                <div className="signature_home">
-                    <img src={Signature} alt="signature" />
-                </div>
-                <AboutImage/>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default AboutUs;
