e802887f13081597012ee4186d9631383e6250be
ICdalia <idalia.cieciura@onet.pl>
2023-07-06 16:51:04 +0200

    section give

---
+++ src/components/giveBack/giveBack.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Decoration from "../../assets/Decoration.svg";
+// import "../styleThree.scss"
+
+const GiveBack = () => {
+    return (
+        <div className="container_giveback">
+            <div className="we_giveback">
+                <div className="title_section">
+                    Oddaj rzeczy, których już nie chcesz POTRZEBUJĄCYM
+                </div>
+                <div className="decoration_help">
+                    <img src={Decoration} alt="Decoration Help" />
+                </div>
+                <div className="subtitle_section">
+                    Wystarczą 4 proste kroki:
+                </div>
+                <div className="foursteps">
+
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default GiveBack;
+
