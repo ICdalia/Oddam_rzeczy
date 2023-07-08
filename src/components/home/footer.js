ff55e53c5954c408bec7cc3779dc2d2a5f5c8f8f
ICdalia <idalia.cieciura@onet.pl>
2023-07-08 23:48:43 +0200

    add footer

--- src/components/home/footer.js
+++ src/components/home/footer.js
@@ -1,16 +1,19 @@
-// import React from "react";
-// import "../stylesHome.scss"
-// import homeImage from "../../assets/homeImage.png";
-//
-// const ContactImage = () => {
-//     return (
-//         <div className="container_structure">
-//             <div className="container_image">
-//                 <img src={homeImage} alt="Home Image" className="image_home"/>
-//             </div>
-//         </div>
-//
-//     )
-// };
-//
-// export default ContactImage;
+import React from "react";
+import "../stylesContact.scss"
+import Facebook from "../../assets/Facebook.png";
+import Instagram from "../../assets/Instagram.png";
+
+const Footer= () => {
+    return (
+        <div className="container_contact_footer">
+            <p className="text_footer">Copyright by Coders Lab</p>
+            <div className="container_image">
+                <img src={Facebook} alt="facebook" className="image_footer"/>
+                <img src={Instagram} alt="instagram" className="image_footer"/>
+            </div>
+        </div>
+
+    )
+};
+
+export default Footer;
